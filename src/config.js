7312f9a99a11606cfae7dcc43806670f2a1c54ea
Adetayo Rabiu <adetayo.rabiu@gmail.com>
2020-07-14 23:05:26 -0400

    setting up react app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-1",
+      BUCKET: "fp-notes-app-uploads"
+    },
+    apiGateway: {
+      REGION: "us-east-1",
+      URL: "https://9ru80nmnp2.execute-api.us-east-1.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-east-1",
+      USER_POOL_ID: "us-east-1_19AXDDDWO",
+      APP_CLIENT_ID: "725ieruhvhg34tr30nhqhjlmf2",
+      IDENTITY_POOL_ID: "us-east-1:2ebd67da-387b-4298-8772-09709601fdde"
+    }
+  };
